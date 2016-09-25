45b55843585cb3f2bc756cd9cddf2a82230309b2
Henry Hamon <henryhamon@gmail.com>
2016-09-25 08:38:12 -0300

    start a first spec

--- spec/snaildb_spec.rb
+++ spec/snaildb_spec.rb
@@ -1,11 +1,25 @@
 require 'spec_helper'
 
 describe Snaildb do
+  before(:each) do
+    # reset all
+  end
+
   it 'has a version number' do
     expect(Snaildb::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  it 'can initialize a new database' do
+  end
+
+  it 'can load a database'
+
+  it 'can create' do
+  end
+
+  it 'can read' do
+  end
+
+  it 'can update' do
   end
 end
